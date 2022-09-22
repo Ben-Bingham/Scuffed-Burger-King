70089ee9cfa3acd6c209f0df570c9508b9195295
Ben-Bingham <94578530+Ben-Bingham@users.noreply.github.com>
2022-09-22 09:48:15 -0600

    work

--- scripts/jobs.js
+++ scripts/jobs.js
@@ -7,3 +7,15 @@ function hired() {
     }
     document.getElementById("getJob").innerHTML = "";
 }
+
+function clearElement() {
+    document.getElementById("loading").innerHTML = "";
+    document.getElementById("video").style.visibility = "visible";
+
+}
+
+function loading() {
+    document.getElementById("loading").innerHTML = "<img src=\"assets/Whopper.png\" class=\"spin\" width=\"100\">";
+    document.getElementById("video").style.visibility = "hidden";
+    setTimeout(clearElement, 1000);
+}
